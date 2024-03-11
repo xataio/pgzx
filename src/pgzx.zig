5d3bdb0aeb85b2f94f1ce280b1db98aa384d2b26
urso <steffen.siering at gmail.com>
2024-03-11 15:50:13 +0100

    Dynahash: init support

--- src/pgzx.zig
+++ src/pgzx.zig
@@ -5,16 +5,32 @@
 
 const std = @import("std");
 
-// pub const Build = @import("pgzx/build.zig");
-
 // Export common set of postgres headers.
 pub const c = @import("pgzx/c.zig");
 
 // Utility functions for working with the PostgreSQL C API.
 pub const bgworker = @import("pgzx/bgworker.zig");
+
+pub const collections = @import("pgzx/collections.zig");
+pub const PointerListOf = collections.list.PointerListOf;
+pub const SList = collections.slist.SList;
+pub const DList = collections.dlist.DList;
+pub const HTab = collections.htab.HTab;
+pub const HTabIter = collections.htab.HTabIter;
+pub const StringHashTable = collections.htab.StringHashTable;
+pub const KVHashTable = collections.htab.KVHashTable;
+
 pub const elog = @import("pgzx/elog.zig");
+
 pub const err = @import("pgzx/err.zig");
+pub const PGError = err.PGError;
+pub const pgRethrow = err.pgRethrow;
+
 pub const fmgr = @import("pgzx/fmgr.zig");
+pub const PG_MODULE_MAGIC = fmgr.PG_MODULE_MAGIC;
+pub const PG_FUNCTION_V1 = fmgr.PG_FUNCTION_V1;
+pub const PG_FUNCTION_INFO_V1 = fmgr.PG_FUNCTION_INFO_V1;
+
 pub const lwlock = @import("pgzx/lwlock.zig");
 pub const mem = @import("pgzx/mem.zig");
 pub const pq = @import("pgzx/pq.zig");
@@ -25,17 +41,4 @@ pub const utils = @import("pgzx/utils.zig");
 pub const intr = @import("pgzx/interrupts.zig");
 pub const testing = @import("pgzx/testing.zig");
 
-// data structures
-pub const collections = @import("pgzx/collections.zig");
-
 pub const guc = utils.guc;
-
-pub const PGError = err.PGError;
-pub const pgRethrow = err.pgRethrow;
-
-pub const PG_MODULE_MAGIC = fmgr.PG_MODULE_MAGIC;
-pub const PG_FUNCTION_V1 = fmgr.PG_FUNCTION_V1;
-pub const PG_FUNCTION_INFO_V1 = fmgr.PG_FUNCTION_INFO_V1;
-
-pub const list = collections.list;
-pub const PointerListOf = list.PointerListOf;
